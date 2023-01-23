f80280cfac461f30804529ab07e41890da609c14
Danny Espinoza <danny.espinozasanchez@fiserv.com>
2023-01-23 09:44:20 -0600

    Add iOS initial code

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Danny Espinoza Sanchez on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
