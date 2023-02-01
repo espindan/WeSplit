9587aa8082d065a8519701d112105b95ae2ee752
Danny Espinoza <danny.espinozasanchez@fiserv.com>
2023-02-01 15:08:55 -0600

    add based form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form{
+                Section {
+                    //Locale is a massive struct built into iOS that is responsible for storing all the user’s region settings what calendar they use, how they separate thousands digits in numbers, whether they use the metric system, and more
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)//numberPad
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                    
+                }
+            }
+            .navigationTitle("WeSplit")
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    // Type some View -> return a view that conforms the View protocol
     static var previews: some View {
         ContentView()
     }
