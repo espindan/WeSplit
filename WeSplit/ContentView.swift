d2d90ab7725e4894a8535d93647e07bb96b1692c
Danny Espinoza <danny.espinozasanchez@fiserv.com>
2023-02-02 08:09:47 -0600

    Add challenges

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -13,18 +13,18 @@ struct ContentView: View {
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
-    let tipPercentages = [10, 15, 20, 25, 0]
+    //let tipPercentages = [10, 15, 20, 25, 0]
+    let currency = FloatingPointFormatStyle<Double>.Currency(code: Locale.current.currencyCode ?? "USD")
     
     var totalPerPerson: Double {
-        
         let peopleCount = Double(numberOfPeople + 2)
+        return grandTotal / peopleCount
+    }
+    
+    var grandTotal: Double {
         let tipSelection = Double(tipPercentage)
-        
         let tipValue = checkAmount / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
-        
-        return amountPerPerson
+        return checkAmount + tipValue
     }
     
     var body: some View {
@@ -32,28 +32,35 @@ struct ContentView: View {
             Form{
                 Section {
                     //Locale is a massive struct built into iOS that is responsible for storing all the user’s region settings what calendar they use, how they separate thousands digits in numbers, whether they use the metric system, and more
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    TextField("Amount", value: $checkAmount, format: currency)
                         .keyboardType(.decimalPad)//numberPad
                         .focused($amountIsFocused)
+                        
                     
                     Picker("Number of people", selection: $numberOfPeople){
                         ForEach(2..<100){
                             Text("\($0) people")
                         }
                     }
-                    
                 }
                 Section{
                     Picker("Tip percentage", selection: $tipPercentage){
-                        ForEach(tipPercentages,id: \.self){
+                        ForEach(0..<101){
                             Text($0,format: .percent)
                         }
-                    }.pickerStyle(.segmented)
+                    }
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
                 Section{
-                    Text(totalPerPerson,format: .currency(code:  Locale.current.currencyCode ?? "USD"))
+                    Text(grandTotal,format: currency)
+                } header: {
+                    Text("Total amount for the check")
+                }
+                Section{
+                    Text(totalPerPerson,format: currency)
+                } header: {
+                    Text("Amount per person")
                 }
             }
             .navigationTitle("WeSplit")
