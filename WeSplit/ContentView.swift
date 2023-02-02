4f0303b7d4529127d04b8b90ccf5e1c7fb135bdb
Danny Espinoza <danny.espinozasanchez@fiserv.com>
2023-02-02 06:34:11 -0600

    improve keyboard experience

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,13 +12,14 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    
+    @FocusState private var amountIsFocused: Bool
     let tipPercentages = [10, 15, 20, 25, 0]
+    
     var totalPerPerson: Double {
         
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
-
+        
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
@@ -33,6 +34,7 @@ struct ContentView: View {
                     //Locale is a massive struct built into iOS that is responsible for storing all the user’s region settings what calendar they use, how they separate thousands digits in numbers, whether they use the metric system, and more
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                         .keyboardType(.decimalPad)//numberPad
+                        .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople){
                         ForEach(2..<100){
@@ -55,6 +57,14 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
         
     }
